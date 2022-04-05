1b8d07dca5cc35c39eae826ce099eb158af4c52a
Flavio Bernaski <fbernaski@gmail.com>
2022-04-05 00:16:16 -0300

    Configuração React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="flexContainer">
+            <p className="alignCenter">Implementar coisas</p>
+        </div>
+    );
+}
+
+export default App;
