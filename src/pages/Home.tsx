d0efe63f5dc4ae24677ef21cc92e7d8ce3172262
vintercardoso <davi.dev@agroflux.ind.br>
2022-04-06 00:47:25 -0300

    Adiciona Router e pagina inicial incompleta

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const styles = {
+    fillImage: {
+        width: '100%',
+    }
+};
+
+const Home = () => {
+
+
+    return (
+        <>
+            <div style={styles.fillImage}>
+                <img src='/resources/HomeHeader.svg' alt="Imagem Topo UTFPR"></img>
+            </div>
+            <div>Teste aaaaaaa</div>
+        </>
+    );
+}
+
+export default Home;
