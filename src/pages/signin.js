a7cffa06765464304f6673c1bff3e0fbd2606f67
CAIO LUIZ DOS SANTOS <a1903683@debian.dacom>
2022-05-02 19:15:18 -0300

    Inicio construçao signin

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,12 +1,48 @@
-import { Box, useColorMode } from "@chakra-ui/react"
+import { Box, useColorMode,Text,Image, Flex } from "@chakra-ui/react"
+import { useState } from "react";
 export default function Home() {
     const { colorMode } = useColorMode();
     const brandColor = {light: 'brand.900',dark: 'brand.700'};
+    const [ isLogin, setIsLogin ] = useState(true);
 
     return (
-        <Box width={"100%"} height={'100vh'}  bgImage={'localhost:3000/vercel.svg'}>
-                SIGNIN
-        </Box>    
+        <Flex 
+        justifyContent={'center'}
+        alignItems="center"
+        position={"relative"}
+        width={ `100%`}
+        h={`100vh`}
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        padding="0"
+        bgSize="cover"
+        bgImage={'/background.png'}>   
+            <Box borderRadius={'10px'} bg={'white'} p={2}>
+                <Flex>
+                    <Text p={2}>
+                        Login
+                    </Text>
+                    <Text p={2}> 
+                        Cadastrar
+                    </Text>
+                    
+                </Flex>
+                { isLogin?
+                    <Box>
+                        LoginForm
+                    </Box>
+                    :
+                    <Box>
+                        CadastroForm
+                    </Box>
+                }
+                
+                
+
+            </Box>
+
+
+        </Flex>    
     )
   }
   
