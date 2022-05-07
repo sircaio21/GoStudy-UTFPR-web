a07fd04d4de530cf996eda8664f02e4cc5873425
unknown <joserenandasilva08@gmail.com>
2022-05-07 17:58:48 -0300

    ajeitando a tela principal e iniciando a tela rooms

--- src/pages/rooms.js
+++ src/pages/rooms.js
@@ -1,7 +1,13 @@
-
+import { Box, Flex,Grid,GridItem,Text, Button } from "@chakra-ui/react"
+import Header from '../components/MainPage/Header'
+import Reservas from "../components/MainPage/Reservas"
+import Informacoes from "../components/MainPage/Informacoes"
 export default function Home() {
     return (
-        <h1>ROOMS</h1>    
+        <Box bgColor={"#E5E5E5"} width={'100%'} height={'100vh'}>
+        <Header/>
+        
+      </Box>   
     )
   }
   
