d12385de3cedd1c5553fda3848cfc81f57cbf58c
CAIO LUIZ DOS SANTOS <a1903683@debian.dacom>
2022-05-23 17:20:28 -0300

    continuação admin page

--- src/components/AdminPage/Register/index.js
+++ src/components/AdminPage/Register/index.js
@@ -1,10 +1,54 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Flex, Text } from "@chakra-ui/react"
 
 export default ()=>{
     
     
     return(
-        <Box bg={'yellow'} height={'90%'}>Cadastro</Box>
+        <Flex p={3} flexDir={'column'} maxWidth={'400px'}  bg={'yellow'} height={'90%'}>
+            <Text fontSize={'24px'} fontWeight='bold'>
+                Cadastros
+            </Text>
+            <Flex 
+            justifyContent={'center'}
+            fontSize={'22'}
+            p={2} 
+            bg={"#E6E6E6"} 
+            width={'80%'}>
+                <Text>Instituição</Text>
+            </Flex>
+            <Flex 
+            justifyContent={'center'}
+            fontSize={'22'}
+            p={2} 
+            bg={"#E6E6E6"} 
+            width={'80%'}>
+                <Text>Horário</Text>
+            </Flex>
+            <Flex 
+            justifyContent={'center'}
+            fontSize={'22'}
+            p={2} 
+            bg={"#E6E6E6"} 
+            width={'80%'}>
+                <Text>Salas</Text>
+            </Flex>
+            <Flex 
+            justifyContent={'center'}
+            fontSize={'22'}
+            p={2} 
+            bg={"#E6E6E6"} 
+            width={'80%'}>
+                <Text>Usuários</Text>
+            </Flex>  
+            <Flex 
+            justifyContent={'center'}
+            fontSize={'22'}
+            p={2} 
+            bg={"#E6E6E6"} 
+            width={'80%'}>
+                <Text>Reservas</Text>
+            </Flex>   
+        </Flex>
     )
 
 }
