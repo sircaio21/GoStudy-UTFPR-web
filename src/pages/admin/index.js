2f1752b929d2bea9f4d26b0e15885d8cd8557764
Caio <caio21122001@gmail.com>
2022-06-05 20:15:17 -0300

    Tela Admin semi pronta

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -5,18 +5,19 @@ import Register from "../../components/AdminPage/Register"
 import ExitBtnPanel from "../../components/AdminPage/ExitBtnPanel"
 export default function Admin() {
     return (
-        <Box width={"100%"} height={"100vh"}>
+        <Box width={"100%"} height={"100vh"} bg={'#F4F4F4'}>
             <Header/>
             <Flex justifyContent={'center'}
             width={'100%'} 
             height={'max-content'}
+            p={4}
             >
-                <Box width={'50%'} >
+                <Flex p={4} width={'50%'} justifyContent={'right'}>
                     <Register/>
-                </Box>
-                <Box width={'50%'}>
+                </Flex>
+                <Flex p={4}  width={'50%'} justifyContent={'left'}>
                     <Visualization/> 
-                </Box>
+                </Flex>
             </Flex>
             <Flex bg={'#696'} justifyContent={'center'} width={'100%'}>
                 <ExitBtnPanel/>
