44acdf4867db6a959465206b4e817475240cd428
unknown <joserenandasilva08@gmail.com>
2022-06-17 16:03:17 -0300

    add telas cadastroreservas, cadastrossalas, cadastrousuarios

---
+++ src/pages/admin/cadastrosalas.js
@@ -0,0 +1,23 @@
+import { Box, Flex,Grid,GridItem,Text, Button } from "@chakra-ui/react"
+import Header from '../../components/Header'
+import Retornar from "../../components/cadastro/retornar"
+import Botoes from "../../components/cadastro/botoes"
+import Inputs from "../../components/cadastro/inputssalas"
+import Horarios from "../../components/RoomPage/Salas/horarios"
+export default function Home() {
+    return (
+        <Box bgColor={"#EEEDEA"} width={'100%'} height={'100vh'}>
+          <Header/>
+          <Box p={4} display = {"flex"} alignItems={"center"} justifyContent = {"center"}>
+            <Retornar titulo={'Salas'}/>
+          </Box>
+          <Box p={4} display = {"flex"} alignItems={"center"} justifyContent = {"center"}>
+            <Inputs/>
+          </Box>
+          <Box p={4} display = {"flex"} alignItems={"center"} justifyContent = {"center"}>
+          <Botoes/>
+          </Box>       
+        </Box>  
+    )
+  }
+  
