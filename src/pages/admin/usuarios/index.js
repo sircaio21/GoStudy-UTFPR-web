56f33c5e1f4cea3d6917fa166576b49428ae0e3d
João Vitor Moraski Lunkes <joaomoraskilunkes@gmail.com>
2022-06-26 16:50:38 -0300

    Update index.js

--- src/pages/admin/usuarios/index.js
+++ src/pages/admin/usuarios/index.js
@@ -13,7 +13,7 @@ export default function Usuarios() {
         <Box bgColor={"#EEEDEA"} width={'100%'} height={'100vh'}>
           <Header/>
           <Box p={4} display = {"flex"} alignItems={"center"} justifyContent = {"center"}>
-            <Retornar titulo={'Campus'}/>
+            <Retornar titulo={'Usuários'}/>
           </Box>
           <Box p={4} display = {"flex"} alignItems={"center"} justifyContent = {"center"}>
             <UsuarioContainer/>          
