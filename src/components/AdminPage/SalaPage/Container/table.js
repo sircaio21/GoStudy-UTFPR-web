847afd63bdf311e0690cf7026103bc0481a898fa
sircaio21 <caio21122001@gmail.com>
2022-06-26 17:24:20 -0300

    Salas

---
+++ src/components/AdminPage/SalaPage/Container/table.js
@@ -0,0 +1,62 @@
+import { Box, Text, Grid, GridItem, Flex, Center, Button, list } from "@chakra-ui/react"
+import { BsFillPencilFill, BsFillTrashFill } from 'react-icons/bs';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
+import React, { useState } from "react";
+import Pagination from '../../Pagination'
+
+const max_itens = 3;
+const max_left = (max_itens -1)/2;
+export default function ContainerCampus({salaList}){
+    const [offset, setoffset] = useState(0);
+    return(
+        <Box borderRadius={'5px'} >
+            <TableContainer>
+            <Table variant='unstyled'>
+            <Thead>
+                <Tr>
+                <Th isNumeric backgroundColor={"#DEEEFF"}>Id</Th>
+                <Th backgroundColor={"#DEEEFF"}>Câmpus</Th>
+                <Th backgroundColor={"#DEEEFF"}>Número</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+            {salaList?.slice(offset,offset+5).map((sala) => ( 
+                        <Tr>
+                            <Td isNumeric>{sala.id}</Td>
+                            <Td>{sala.fk_id_institute}</Td>
+                            <Td>{sala.number}</Td>
+                            <td>
+                                <Button variant='ghost' colorScheme='none' 
+                                border={'Background'} alignContent={'center'} 
+                                justifyContent={'center'}  marginRight={1} 
+                                padding={0}>
+                                    {<BsFillPencilFill />}
+                                </Button>
+                                <Button variant='ghost' colorScheme='none' padding={-1}>
+                                    {<BsFillTrashFill />}
+                                </Button>
+                            </td>
+                        </Tr> 
+                ))}
+            </Tbody>
+            </Table>
+        </TableContainer>
+        <Pagination 
+            limit={5} 
+            total={salaList?.length} 
+            offset={offset}
+            setoffset={setoffset}
+        />
+    </Box>
+    )
+}
---
+++ src/pages/admin/salas/index.js
@@ -0,0 +1,58 @@
+
+import getUserById from "../../../services/user/getUserById"
+import { parseCookies, destroyCookie } from "nookies"
+import jwt_decode from "jwt-decode"
+import { Box, Flex,Grid,GridItem,Text, Button } from "@chakra-ui/react"
+import Header from '../../../components/Header'
+import Retornar from "../../../components/AdminPage/SalaPage/retornar"
+import UsuarioContainer from "../../../components/AdminPage/SalaPage/Container"
+
+export default function Salas() {
+    return (
+        <Box bgColor={"#EEEDEA"} width={'100%'} height={'100vh'}>
+          <Header/>
+          <Box p={4} display = {"flex"} alignItems={"center"} justifyContent = {"center"}>
+            <Retornar titulo={'Salas'}/>
+          </Box>
+          <Box p={4} display = {"flex"} alignItems={"center"} justifyContent = {"center"}>
+            <UsuarioContainer/>          
+          </Box>    
+        </Box>  
+    )
+}
+
+export const getServerSideProps = async (ctx) => {
+    const { 'gostudy-token': token } = parseCookies(ctx);
+    if (!token) {
+        return {
+            redirect: {
+                destination: '/signin',
+                permanent: false,
+            }
+        }
+    }
+    let decodedToken = jwt_decode(token);   
+    if(decodedToken.id){
+        const response = await getUserById({id: decodedToken.id, token: token})
+        if(response.status != 'success'){
+            destroyCookie({}, 'gostudy-token');
+            return {
+                redirect: {
+                    destination: '/signin',
+                    permanent: false,
+                }
+            }
+        }
+        if(!response.data.isAdmin){
+            return {
+                redirect: {
+                    destination: '/',
+                    permanent: false,
+                }
+            }
+        }
+    }
+    return {
+        props: {}
+    }
+}
